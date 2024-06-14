0ee49a00fd8bad45f33bcba9b70d735d76dababd
Luis Eduardo <luiggis@hotmail.com>
2024-06-13 20:22:31 -0500

    1.01

--- src/components/cartWidget/CartWidget.jsx
+++ src/components/cartWidget/CartWidget.jsx
@@ -3,6 +3,22 @@ const CartWidget = () => {
   return (
     <div id="cart">
       <FaShoppingCart color="white" size={30} />
+      <span 
+        style={{
+          position: 'absolute',
+          top: '25px',
+          right: '-10px',
+          backgroundColor: 'red',
+          color: 'white',
+          borderRadius: '50%',
+          padding: '5px 10px',
+          fontSize: '12px'
+        }}
+      >
+        5
+      </span>
+
+
     </div>
   );
 };
